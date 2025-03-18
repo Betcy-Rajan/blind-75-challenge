ee73f6dbfd712713c845ec95bb7c2fe7471a8487
Betcy-Rajan <betcyrajan01@gmail.com>
2025-03-19 00:56:26 +0530

    Added solution for Two Sum

---
+++ Challenge-1/two-sum.py
@@ -0,0 +1,35 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+# Constraints:
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+
+def twoSum(nums, target):
+    Sums={}
+    for i in range(len(nums)):
+        n=nums[i]
+        rem=target-n
+        if rem in Sums:
+            return [Sums[rem],i]
+        Sums[n]=i
+    return [-1,-1]
+
+nums=list(map(int,input("Enter the numbers:").split()))
+target=int(input("Enter the target:"))
+print(twoSum(nums,target))
